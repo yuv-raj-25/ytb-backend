fb9eeb49b227cbf601cb93da9b3006abb0647b5c
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2024-08-30 13:39:56 +0530

    feat: user & video model added

---
+++ learnBackend/src/models/user.models.js
@@ -0,0 +1,93 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true , "password is required "],
+        unique: true
+    },
+    avatar: {
+        type: String,  // cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    refreshToken: {
+        type: String
+    }
+
+},{timestamps: true})
+
+userSchema.pre("save" , async function(next){
+
+    if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(pass){
+     return await bcrypt.compare(pass , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+export const User = mongoose.model('User', userSchema)
---
+++ learnBackend/src/models/video.models.js
@@ -0,0 +1,54 @@
+import mongoose , {Schema} from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true,
+
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+
+        },        
+        tittle: {
+            type: String,
+            required: true,
+
+        },        
+        discription: {
+            type: String,
+            required: true,
+
+        },        
+        duration: {
+            type: Number,  // cloudinary url
+            required: true,
+        },        
+        views: {
+            type: Number,
+            default: 0,
+        },        
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },        
+    },   
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Video = mongoose.model("Video" , videoSchema)
